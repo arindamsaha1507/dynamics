9cfdcf9172448ad929c68e03091cf5d5e0022f28
Arindam Saha <arindamsaha1507@gmail.com>
2022-12-20 08:47:36 +0530

    Added resource ode system

--- configuration/ode.py
+++ configuration/ode.py
@@ -4,6 +4,8 @@ def ode(x, t, p, type):
 
     if type == 'fhn':
         return fhn(x, p)
+    elif type == 'resource':
+        return resource(x, p)
 
 def fhn(x, p):
     
@@ -12,4 +14,12 @@ def fhn(x, p):
     return np.array([
         x[0] - (x[0]**3)/3.0 - x[1] + 0.5,
         (x[0] + p['a'] - p['b']*x[1])/p['c']
+    ])
+
+def resource(x, p):
+
+    assert len(x) == 1
+
+    return np.array([
+        p['r']*x[0]*(1.0-x[0]) - p['p']*x[0]
     ])
