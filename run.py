4444152f3b32127c8b80eec4a7df24b0ac67b8d4
Arindam Saha <arindamsaha1507@gmail.com>
2022-12-28 16:20:10 +0000

    Added flag for eigenvalue computations

--- run.py
+++ run.py
@@ -11,6 +11,8 @@ if __name__=='__main__':
     parser.add_argument('--random_params', action="store", default="None", help="Randomizes parameters")
     parser.add_argument('--random_ic', action="store", default="None", help="Randomizes initial conditions")
 
+    parser.add_argument('--eigen', action="store_true", help='Flag to compute eigenvalues')
+
     parser.add_argument('-o', '--outfile', action="store", default="timeseries.csv", help="The output file where timeseries is stored")
     parser.add_argument('-e', '--evfile', action="store", default="eigenvalues.csv", help="The output file where eigenvalues is stored")
 
@@ -18,6 +20,8 @@ if __name__=='__main__':
 
     S = System(args)
     S.solver()
-    S.eigenvalues()
     S.save_timeseries(args.outfile)
-    S.save_eigenvalues(args.evfile)
+
+    if args.eigen:
+        S.eigenvalues()
+        S.save_eigenvalues(args.evfile)
